721be821110dac8ca28c58ac0a02b78e5caaf4fe
michal-wq <ka.michal@outlook.com>
2025-03-01 18:17:14 +0100

    training.py erstellt

---
+++ training.py
@@ -0,0 +1,7 @@
+"""
+Hier wird:
+- feautre engineering automatisch durchgeführt, die Dateien werden nicht gespeichert sondern nur als Dataframe instanziert
+und fürs Training von ML Modellen verwendet
+- ML Modell wird dann gespeichert
+- Es wird (wahrscheinlich) nicht in dieser Datei getestet.
+"""
