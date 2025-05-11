b377ee969e137c447f78750de2bd6416d7f8a5eb
filip <filipvrlec05@gmail.com>
2025-05-11 22:52:27 +0200

    did something, will do tomorrow more

--- website/pages/home.py
+++ website/pages/home.py
@@ -1,11 +1,16 @@
-from dash import Dash, html, dcc
+from dash import html
 
 layout = html.Div(
     [
         html.Img(src="./assets/img/Logo + Title.svg"),
         html.Div(
             [
-                html.Button("Find the perfect match", className="button-primary"),
+                html.Button(
+                    "Find the perfect match",
+                    className="button-primary",
+                    id="change-search-view",
+                    n_clicks=0,
+                ),
                 html.Button("Imprint", className="button-secondary"),
             ],
             className="homepage-button-container",
@@ -13,3 +18,7 @@ layout = html.Div(
     ],
     className="homepage-content",
 )
+
+
+def get_layout():
+    return layout
