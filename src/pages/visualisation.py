a64f9b94318fb1f6bc895f01a2976fd5d1733a12
filip <filipvrlec05@gmail.com>
2025-06-22 18:14:45 +0200

    balkaaaa (michau)

--- src/pages/visualisation.py
+++ src/pages/visualisation.py
@@ -3,6 +3,9 @@ from dash.dependencies import Output, Input, State, ALL
 import pandas as pd
 import src.state as state
 
+# und o no an liniadiagramm wo zeigt welche genres am meista gmacht wora sind im jeweiliga johr oder so
+# zum beispiel an balka diagramm, wo s mean rating zeigt vo allna film vo jeweilige genres
+
 
 def get_layout():
     return html.Div("MEOW")
